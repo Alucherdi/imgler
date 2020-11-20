d1b7e4b1c1721d6edc6c7494d426e4c5f765cc69
Alucherdi <alucherdi@pop-os.localdomain>
2020-11-20 17:05:52 -0600

    Dont forget the require

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = module.exports = express()
 app.use("/imgler/assets/", express.static('public'));
 
 app.use(require("./src/image/image.router"))
+app.use(require("./src/file/file.router"))
 
 app.listen(5432, () => {
     console.log("Listening to 5432")
